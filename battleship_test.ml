0e0a2cba59bb9865d1e526b495b7cc26d6f80958
Mathieu Icapi <130665549+insanemat@users.noreply.github.com>
2025-03-14 17:51:17 +0100

    Update battleship_test.ml

---
+++ battleship_test.ml
@@ -0,0 +1,65 @@
+#use "CPtest.ml";;
+
+(**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+
+Vérifie si la marge est égale a 30 pixels 
+    *)
+test_fonc_1_init_params_margin(): unit =
+;;
+
+(**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+Vérifie si la grille est égale a 15
+    *)
+    test_fonc_1_init_params_cell_size(): unit =
+;;
+
+(**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+
+Vérifie si la hauteur de la zonz est égale a 60 pixels matérialiés en bleu
+    *)
+    test_fonc_1_init_params_message_size(): unit =
+  ;;
+
+  (**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+Vérifie si les 2 grilles ont 10 cases 
+    *)
+test_fonc_1_init_params_grid_size(): unit =
+;;
+
+(**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+Vérifie si la fenetre est bien ouverte *)
+ test_fonc_battleship_game_open_graph():unit=
+
+ (**@author Anne Celia Mensah 
+@author Maël ICAPI
+@return unit
+-----------
+    *)
+    test_fonc_battleship_set_window_title():unit=
+;;
+
+
+  (**Vérifie si la fenetre soit de la bonne taille
+@author Anne Celia Mensah 
+@author Maël ICAPI 
+@return unit  *)
+    test_fonc_battleship_size_x():unit=
+;;
+
+(** Vérifie si la fenetre soit de la bonne taille
+@author Anne Celia Mensah 
+@author Maël ICAPI 
+@return unit
+    *)
+    test_fonc_battleship_size_y():unit=
+;;
