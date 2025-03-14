fcada346422d93958af503be4994f8522faa7f18
Yuo_7 <mariusroumy@gmail.com>
2025-03-14 17:16:26 +0100

    Update battleship.ml
    Ecriture du type t_params
    + les signatures des fonctions et la documentation

--- battleship.ml
+++ battleship.ml
@@ -1,2 +1,50 @@
-(**
+(** Ce fichier est notre fichier source 
+@author Sarah Favre
+@author Marius Roumy
+@author Anne-Celia Mensah
+@author Maêl Icapi
+
+@version 1.0
 *)
+
+(** Le type t_params est le type structuré pour nos paramètre
+@author Sarah Favre
+@author Marius Roumy
+*)
+type t_params = {margin : int ; cell_size : int ; grid_size : int ; window_width : int ; window_height : int ; message_size : int} 
+(*margin = marge autour des grilles ; cell_size = taille des cases ; grid_size = nombres de cases par côté
+; window_width = largeur totale de la fenêtre ; window_height = hauteur totale de la fenêtre ; message_size = taille des caractères *)
+;;
+
+(** la fonction initialise nos paramètres
+@author Sarah Favre
+@author Marius Roumy
+@param p_margin marge autour des grilles
+@param p_cell_size taille des cases
+@param p_grid_size nombres de cases par côté
+@param p_window_width largeur totale de la fenêtre
+@param p_window_height hauteur totale de la fenêtre
+@param p_message_size taille des caractères
+@return t_params
+*)
+let init_params(p_margin, p_cell_size, p_grid_size, p_window_width, p_window_height, p_message_size : int * int * int * int * int * int): t_params =
+
+;;
+
+(**la fonction permet d'afficher les deux grilles vides et le nom des deux joueurs
+@author Sarah Favre
+@author Marius Roumy
+@param p_params nos paramètres 
+@return unit
+*)
+let display_empty_grids(p_params : t_params): unit =
+
+;;
+
+(**la fonction permet d'ouvrir la fenètre graphique aux dimensions appropriées, mettre a jour son titre
+et effectuer les affichages adéquates.
+@author Sarah Favre
+@author Marius Roumy
+@return unit
+*)
+let battleship_game(): unit =
